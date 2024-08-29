7616491054ef1105d22383b933bb7cc078a3f83e
Manuel Grgic Figueiredo <manel.gf.95@gmail.com>
2024-08-29 23:15:17 +0200

    add input field

---
+++ src/components/Input.svelte
@@ -0,0 +1,27 @@
+<input type="text">
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
+
+    input {
+        box-sizing: border-box;
+        border-radius: 50px;
+        background-color: #282B30;
+        color: #CAB2FB;
+        width: calc(100% - 5rem);
+        margin: 1rem 5rem;
+        margin-top: auto;
+        padding: 0.5rem 1rem;
+        border: solid #1E2124 1px;
+        font-family: "Lato", sans-serif;
+        font-weight: 700;
+        font-style: normal;
+        font-size: 20px;
+    }
+
+    input:focus {
+        outline: none !important;
+        border: solid #1B1212 1px;
+        box-shadow: 0 0 10px #424549;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Input from "../components/Input.svelte";
+
+
+</script>
+
+<div id="page">
+    <Input />
+</div>
+
+<style>
+#page {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
